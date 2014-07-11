0958abfcd30dedc25f14b189c46aabffed3d88d9
Óscar Zafra <oskyar@gmail.com>
2014-07-12 00:07:37 +0200

    - Añadido MIDI que faltaba a los sonidos

---
+++ PracticaFinal/src/practicaFinal/filefilter/MIDISoundFilter.java
@@ -0,0 +1,13 @@
+package practicaFinal.filefilter;
+
+/**
+ *
+ * @author oskyar
+ */
+public class MIDISoundFilter extends SoundFileFilter {
+
+    @Override
+    public String getDescription() {
+        return "MIDI files (*.midi)";
+    }
+}
